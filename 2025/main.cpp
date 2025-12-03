72c1e30be8a62481bda835bbe419240089fc6474
Sam Yu <sam.yu@sam.yu-70VM>
2025-12-03 15:43:47 +0000

    Added timing markers

--- 2025/main.cpp
+++ 2025/main.cpp
@@ -1,9 +1,23 @@
 #include <functional>
 #include <iostream>
+#include <chrono>
 
 #include "solutions/Day1.h"
 #include "solutions/Day2.h"
 #include "solutions/Day3.h"
+
+void profile(std::function<void()> function) {
+    auto begin = std::chrono::high_resolution_clock::now();
+    function();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration_micro = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+    auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration_micro);
+    std::cout << "Function took " <<  duration_micro.count() << " μs" << " / " << duration_ms << std::endl;
+
+}
+
+
 
 
 int main() {
@@ -11,18 +25,15 @@ int main() {
         []() { day1(); },
         []() { day2(); },
         []() { day3(); }
-
     };
 
 
-    // for (size_t i = 0; i < functions.size(); ++i) {
-    //     functions[i]();
-    // }
+    for (size_t i = 0; i < functions.size(); ++i) {
+        std::cout << "=====================" << std::endl;
+        std:: cout << "Day " << (i+1) << std::endl;
+        profile(functions[i]);
+    }
 
-    int run = 2;
-
-
-    functions[run]();
 
 
     return 0;
