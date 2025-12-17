f6fdd309bcad2e861da989496e89ca68271c5ba2
Sam Yu <samchenyu@gmail.com>
2025-12-17 17:53:00 +0000

    Adjusted for my old intel mac

--- 2025/main.cpp
+++ 2025/main.cpp
@@ -24,7 +24,7 @@ void profile(std::function<void()> function) {
 
     auto duration_micro = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
     auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration_micro);
-    std::cout << "Function took " <<  duration_micro.count() << " us" << " / " << duration_ms << std::endl;
+    std::cout << "Function took " <<  duration_micro.count() << " us" << " / " << duration_ms.count() << " ms" << std::endl;
 
 }
 
